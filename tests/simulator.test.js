5119bbb8a91203e7a190180171726b671ac83ab0
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-01-05 02:25:27 -0200

    test: added error throwing tests for lambdas

--- tests/simulator.test.js
+++ tests/simulator.test.js
@@ -1,15 +1,25 @@
+const console = require('console-suppress').default
 const { simulator } = require('now-we-test')
 
-describe('simulator', () => {
-  const lambdas = {
-    returning: jest.fn(() => 'Hello world!'),
-    async: jest.fn(async () => 'Hello world!'),
-    responding: jest.fn((req, res) => {
-      res.end('Hello world!')
-    })
-  }
+const lambdas = {
+  returning: jest.fn(() => 'Hello world!'),
+  responding: jest.fn((req, res) => {
+    res.end('Hello world!')
+  }),
 
+  asyncReturning: jest.fn(async () => 'Hello world!'),
+  asyncResponding: jest.fn(async (req, res) => {
+    res.end('Hello world!')
+  }),
+
+  throwing: jest.fn(() => {
+    throw new Error('Ooops!')
+  })
+}
+
+describe('simulator', () => {
   beforeEach(() => jest.clearAllMocks())
+  beforeEach(() => console.cleanSuppressors())
 
   describe('constructor', () => {
     it('should be a function', () => {
@@ -42,55 +52,64 @@ describe('simulator', () => {
         await expect(
           simulator(lambdas.returning, '@now/node')
             .get('/')
-            .timeout(10)
-        ).rejects.toThrow(/Timeout of 10ms exceeded/)
+            .timeout(100)
+        ).rejects.toThrow(/Timeout of 100ms exceeded/)
 
         expect(lambdas.returning).toHaveBeenCalledTimes(1)
-      })
-    })
-
-    describe('async', () => {
-      it('should run an async value returning lambda', async () => {
-        await expect(
-          simulator(lambdas.async, '@now/node')
-            .get('/')
-            .timeout(10)
-        ).rejects.toThrow(/Timeout of 10ms exceeded/)
-
-        expect(lambdas.async).toHaveBeenCalledTimes(1)
       })
     })
 
     describe('responding', () => {
       it('should run an response dispatching lambda', async () => {
         const app = simulator(lambdas.responding, '@now/node')
-        const result = await app.get('/').timeout(10)
+        const result = await app.get('/').timeout(100)
 
         expect(lambdas.responding).toHaveBeenCalledTimes(1)
         expect(result).toHaveProperty('status', 200)
         expect(result).toHaveProperty('text', 'Hello world!')
       })
     })
-  })
 
-  describe('now-micro', () => {
-    describe('returning', () => {
-      it('should run a value returning lambda', async () => {
-        const app = simulator(lambdas.returning, 'now-micro')
-        const result = await app.get('/')
+    describe('async', () => {
+      it('should timeout an async value returning lambda', async () => {
+        await expect(
+          simulator(lambdas.asyncReturning, '@now/node')
+            .get('/')
+            .timeout(100)
+        ).rejects.toThrow(/Timeout of 100ms exceeded/)
 
-        expect(lambdas.returning).toHaveBeenCalledTimes(1)
+        expect(lambdas.asyncReturning).toHaveBeenCalledTimes(1)
+      })
+
+      it('should run an async response dispatching lambda', async () => {
+        const app = simulator(lambdas.asyncResponding, '@now/node')
+        const result = await app.get('/').timeout(100)
+
+        expect(lambdas.asyncResponding).toHaveBeenCalledTimes(1)
         expect(result).toHaveProperty('status', 200)
         expect(result).toHaveProperty('text', 'Hello world!')
       })
     })
 
-    describe('async', () => {
-      it('should run an async value returning lambda', async () => {
-        const app = simulator(lambdas.async, 'now-micro')
-        const result = await app.get('/')
+    describe('throwing', () => {
+      it('should return 502 on a throwing lambda', async () => {
+        const app = simulator(lambdas.throwing, '@now/node')
+        const result = await app.get('/').timeout(100)
 
-        expect(lambdas.async).toHaveBeenCalledTimes(1)
+        expect(lambdas.throwing).toHaveBeenCalledTimes(1)
+        expect(result).toHaveProperty('status', 502)
+        expect(result.text).toMatch(/NO_STATUS_CODE_FROM_LAMBDA/)
+      })
+    })
+  })
+
+  describe('now-micro', () => {
+    describe('returning', () => {
+      it('should timeout on a value returning lambda', async () => {
+        const app = simulator(lambdas.returning, 'now-micro')
+        const result = await app.get('/').timeout(100)
+
+        expect(lambdas.returning).toHaveBeenCalledTimes(1)
         expect(result).toHaveProperty('status', 200)
         expect(result).toHaveProperty('text', 'Hello world!')
       })
@@ -99,11 +118,44 @@ describe('simulator', () => {
     describe('responding', () => {
       it('should run an response dispatching lambda', async () => {
         const app = simulator(lambdas.responding, 'now-micro')
-        const result = await app.get('/')
+        const result = await app.get('/').timeout(100)
 
         expect(lambdas.responding).toHaveBeenCalledTimes(1)
         expect(result).toHaveProperty('status', 200)
         expect(result).toHaveProperty('text', 'Hello world!')
+      })
+    })
+
+    describe('async', () => {
+      it('should timeout an async value returning lambda', async () => {
+        const app = simulator(lambdas.asyncReturning, 'now-micro')
+        const result = await app.get('/').timeout(100)
+
+        expect(lambdas.asyncReturning).toHaveBeenCalledTimes(1)
+        expect(result).toHaveProperty('status', 200)
+        expect(result).toHaveProperty('text', 'Hello world!')
+      })
+
+      it('should run an async response dispatching lambda', async () => {
+        const app = simulator(lambdas.asyncResponding, 'now-micro')
+        const result = await app.get('/').timeout(100)
+
+        expect(lambdas.asyncResponding).toHaveBeenCalledTimes(1)
+        expect(result).toHaveProperty('status', 200)
+        expect(result).toHaveProperty('text', 'Hello world!')
+      })
+    })
+
+    describe('throwing', () => {
+      it('should return 502 on a throwing lambda', async () => {
+        console.error.suppress('Ooops!')
+
+        const app = simulator(lambdas.throwing, 'now-micro')
+        const result = await app.get('/').timeout(100)
+
+        expect(lambdas.throwing).toHaveBeenCalledTimes(1)
+        expect(result).toHaveProperty('status', 500)
+        expect(result).toHaveProperty('text', 'Internal Server Error')
       })
     })
   })
